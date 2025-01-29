7dcb04e318e5dd50f2e83a94ddc14c5d41b3bfbf
Demari Miller <demarijmiller@gmail.com>
2025-01-29 18:07:05 -0500

    deployable

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -5,6 +5,10 @@
 	let firstName = 'John';
 	let lastName = 'Doe';
 	let workEmail = 'sdw@dfw.ce';
+	let personalEmail = 'sdw@dfw.ce';
+	let employerDistrict = 'Baltimore County Public Schools';
+	let school = '';
+	let mobileNumber = '3';
 
 	async function init() {
 		const theme = await fetch(
@@ -12,18 +16,20 @@
 		).then((response) => response.json());
 
 		// Ensure window.botpress is defined (may need <script src="...botpress.bundle.js"> in index.html)
-		window.botpress.on('webchat:ready', () => {
-			window.botpress.open();
-			window.botpress.updateUser({
-				firstName,
-				lastName,
-				workEmail
-			});
-		});
-
 		window.botpress.init({
 			theme,
 			clientId: '0bcc6702-fe27-4a3d-b149-0710b414996b',
+			user: {
+				data: {
+					firstName: firstName,
+					lastName: lastName,
+					email: workEmail,
+					personalEmail: personalEmail,
+					employerDistrict: employerDistrict,
+					school: school,
+					mobileNumber: mobileNumber
+				}
+			},
 			botId: '34ffb0c3-deb4-4b2f-9906-e80e1b5fc39c',
 			style:
 				'https://mediafiles.botpress.cloud/34ffb0c3-deb4-4b2f-9906-e80e1b5fc39c/webchat/v2/style.css',
@@ -53,13 +59,19 @@
 				}
 			}
 		});
+
+		window.botpress.open();
 	}
 
 	onMount(() => {
 		const params = new URLSearchParams(window.location.search);
 		firstName = params.get('firstName') || '';
-		lastName = params.get('lscripte') || '';
+		lastName = params.get('lastName') || '';
 		workEmail = params.get('workEmail') || '';
+		personalEmail = params.get('personalEmail') || '';
+		employerDistrict = params.get('employerDistrict') || '';
+		school = params.get('school') || '';
+		mobileNumber = params.get('mobileNumber') || '';
 		init();
 	});
 </script>
